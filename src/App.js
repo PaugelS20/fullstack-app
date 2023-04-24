4fce4836f618992a0a5b7f0685e9a019e10cf7c6
Sam Paugel <shpaugel@madisoncollege.edu>
2023-04-23 22:07:28 -0500

    Create react app
    installed amplify
    did amplify init
    then did amplify add auth
    the did amplify add hosting
    imported amplify
    then amplify published it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>Hello World!</h1>
+				<Authenticator>
+					{({ signOut, user }) => (
+						<main>
+							<h1>Profile</h1>
+							<h2>Username: {user.username}</h2>
+							<h3>Email: {user.email}</h3>
+							<button onClick={signOut}>Sign out</button>
+						</main>
+					)}
+				</Authenticator>
+			</header>
+		</div>
+	);
+};
 export default App;
